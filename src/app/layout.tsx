31685ebf558f7edbcae1b26caabe033f6ae9d4c6
Nur Muhamad Soleh <nursoleh33@gmail.com>
2025-01-17 13:28:27 +0700

    update compay profile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,21 +5,10 @@ import "swiper/css/scrollbar";
 import "./globals.css";
 import "./fonts.css"
 
-import { Geist, Geist_Mono } from "next/font/google";
-
 import Footer from '@/components/Footer';
 import { LayoutProvider } from './LayoutProvider';
 import type { Metadata } from "next";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 export const metadata: Metadata = {
   title: "PT.DIKA",
   description: "PT.DIKA",
@@ -32,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable}`}
-      >
+      <body>
         <Footer>
           <LayoutProvider>
           {children}
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,13 @@
+"use client"
+
 import { Button, Input as InputAntd } from 'antd'
 
 import Image from 'next/image'
 import React from 'react'
 import { SearchOutlined } from '@ant-design/icons'
+import WhatsAppDika from "../assets/image/whatsApp.png"
+import WhatsAppDikaDesktop from "../assets/image/hallodikawhatsapp.png"
+import {useRouter} from "next/navigation"
 
 interface IProps {
     className?: string
@@ -15,7 +20,7 @@ interface IProps {
 }
 export default function HeroSection(props: IProps) {
     const {className, description, image, title, heading, searchKarir, buttonText} = props
-    
+    const router = useRouter()
     return (
     <section className={`bg-[rgb(27,165,227)] text-white overflow-hidden ${className} -z-50 h-screen flex items-end`}>
         <div className="px-11 w-full">
@@ -48,6 +53,10 @@ export default function HeroSection(props: IProps) {
                     {buttonText && (<Button className="bg-gradient-to-r from-[#FFDB0C] via-[#FFCC0C] to-[#FFB90C] text-lg px-8 py-6 font-bold w-full md:w-auto">
                         DISKUSIKAN SEKARANG
                     </Button>)}
+                    <div className='relative'>
+                        <Image src={WhatsAppDika} alt={"WhatsApp Dika"} className="object-fill bg-transparent fixed top-3/4 right-0 z-50 cursor-pointer md:hidden block" width={100} height={100} onClick={() => window.open("https://wa.me/628118886325","_blank")} />
+                        <Image src={WhatsAppDikaDesktop} alt={"WhatsApp Dika"} className="object-fill bg-transparent fixed top-3/4 right-0 z-50 cursor-pointer md:block hidden" width={250} height={250} onClick={() => window.open("https://wa.me/628118886325","_blank")} />
+                    </div>
                 </div>
             </div>
         </div>
